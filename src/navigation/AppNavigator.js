255b05690a06b9d9a31265dfdf59d890ef9e4ea9
Avi Nerenberg <devavmeister@gmail.com>
2018-12-28 17:58:12 -0500

    minor edits — need to find an alternative to require() cycles for assets (maybe require globally)

--- src/navigation/AppNavigator.js
+++ src/navigation/AppNavigator.js
@@ -1,24 +1,19 @@
 import React from 'react';
-import { Platform, Text } from 'react-native';
+import { Text } from 'react-native';
 import {
 	createAppContainer,
 	createStackNavigator,
-	createTabNavigator,
-  createBottomTabNavigator
+	createBottomTabNavigator
 } from 'react-navigation';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 
 // screens
 import LoginScreen from '../screens/LoginScreen';
-import LogoutScreen from '../screens/LogoutScreen';
 import HomeScreen from '../screens/HomeScreen';
 import AboutUsScreen from '../screens/AboutUsScreen';
 import PlaceOrderScreen from '../screens/PlaceOrderScreen.js';
 import MyAccountScreen from '../screens/MyAccountScreen';
 import RegisterSubscriberScreen from '../screens/RegisterSubscriber';
-import OfferSummaryScreen from '../screens/OfferSummaryScreen';
-import OfferDetailsScreen from '../screens/OfferDetailsScreen';
-import ConfirmOrderScreen from '../screens/ConfirmOrderScreen';
 import AddMealScreen from '../screens/AddMealScreen';
 
 // TODO: organize & code such that tabs change based on user type
@@ -33,8 +28,9 @@ export const SubscriberTabNavigator = createBottomTabNavigator(
 	},
 	{
 		defaultNavigationOptions: ({ navigation }) => ({
-			tabBarLabel: ({ focused, tintColor }) => {
+			tabBarLabel: () => {
 				const { routeName } = navigation.state;
+				// eslint-disable-next-line max-len
 				const tabBarStyle = { textAlign: 'center', /*color: {tintColor},*/ };	// for some reason, tabBarLabel is not styled the same as it is when using .navigationOptions
 				let text;
 
@@ -43,10 +39,11 @@ export const SubscriberTabNavigator = createBottomTabNavigator(
 					case 'AddMeal': 		text = 'Add Meal'; break;
 					case 'MyAccount': 	text = 'My Account'; break;
 					case 'AboutUs': 		text = 'About Us'; break;
+					default: text = ''; break;
 				}
 				return <Text style={tabBarStyle} size={12}>{text}</Text>;
 			},
-			tabBarIcon: ({ focused, tintColor }) => {
+			tabBarIcon: ({ tintColor }) => {
 				const { routeName } = navigation.state;
 				let iconName;
 
@@ -55,6 +52,7 @@ export const SubscriberTabNavigator = createBottomTabNavigator(
 					case 'AddMeal': 		iconName = 'ios-add'; break;
 					case 'MyAccount': 	iconName = 'ios-person'; break;
 					case 'AboutUs': 		iconName = 'ios-information-circle'; break;
+					default: iconName = ''; break;
 				}
 				return <Ionicons name={iconName} size={26} color={tintColor} />;
 			},
@@ -63,13 +61,15 @@ export const SubscriberTabNavigator = createBottomTabNavigator(
 );
 
 // stack navigator (for navigating splash screen/login)
-export const StackNavigator = createStackNavigator({
-	Home: HomeScreen,
-  SignIn: LoginScreen,
-	SignUp: RegisterSubscriberScreen,
-	AboutUs: AboutUsScreen,
+export const StackNavigator = createStackNavigator(
+	{
+		Home: HomeScreen,
+		SignIn: LoginScreen,
+		SignUp: RegisterSubscriberScreen,
+		AboutUs: AboutUsScreen,
 
-	Tabs: SubscriberTabNavigator,
-});
+		Tabs: SubscriberTabNavigator,
+	}
+);
 
 export default createAppContainer(StackNavigator);
--- src/styles/Style.js
+++ src/styles/Style.js
@@ -1,11 +1,11 @@
-import {StyleSheet, Dimensions} from 'react-native';
+import { StyleSheet, Dimensions } from 'react-native';
 
 export const dimensions = {
   fullHeight: Dimensions.get('window').height,
   fullWidth: Dimensions.get('window').width
-}
+};
 
 export const buttons = {
-	backgroundColor: "#f44242",
+	backgroundColor: '#f44242',
 	margin: 5,
-}
+};
